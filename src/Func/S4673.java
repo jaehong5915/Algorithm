bb9b5e8e08efb357048c0e726eda72c3c187be7b
jaehong5915 <woghdclf@naver.com>
2022-01-26 23:18:14 +0900

    .gitignore 생성

--- src/Func/S4673.java
+++ src/Func/S4673.java
@@ -2,14 +2,28 @@ package Func;
 
 public class S4673 {
     public static void main(String[] args) {
-        int n;
-        for(int i=1; i<=10000; i++){
-            if(i < 10 && i%2 == 0){
-                System.out.println(i);
-            }
-            else{
-                
+        boolean [] a =  new boolean[10001];
+
+        for (int i= 1; i<=10000; i++){
+            int n = d(i);
+
+            if( n < 10001){
+                a[n] = true;
             }
         }
+
+        for (int i=1; i<10001; i++){
+            if(a[i] == false){
+                System.out.println(i);
+            }
+        }
+    }
+    public static int d(int number){
+        int sum = 0;
+        while(number != 0){
+            sum += number%10;
+            number = number/10;
+        }
+        return sum;
     }
 }
